1f222c734f62e1c2f6befd68fa17eb0a2186cba4
Jesse Lu <mr.jesselu@gmail.com>
2011-09-26 12:37:54 -0700

    1-way waveguide excitation works for simulator.

--- em_2dte/sim.py
+++ em_2dte/sim.py
@@ -1,43 +1,29 @@
 import petsc4py
-petsc4py.init('-ksp_type preonly -pc_type lu -ksp_monitor')
 from petsc4py import PETSc
 import numpy as np
 import wgmode
 
 class FieldSolve:
-    """ Used to solve Maxwell's equations in two-dimensions (TE mode).
+    """ Holds the result of a simulation of Maxwell's equations.
 
     """
 
-    def __init__(self, field):
-        """ Create the matrices that we will use to simulate different 
-            dielectric structures.
+    def __init__(self, omega, mode_order, epsilon, field, res_norm):
+        """ Store the relevant parameters of the simulation.
 
         """
+        self.omega = omega
+        self.mode_order = mode_order
+        self.epsilon = epsilon
         self.field = field 
+        self.res_norm = res_norm
 
-
-
-    def solve(self, omega, max_iters=1000):
-        self.A.shift(-omega**2)
-
-        self.ksp = PETSc.KSP().create()
-        self.ksp.setOperators(self.A)
-        self.ksp.setFromOptions()
-#         self.ksp.setType('preonly')
-#         print self.ksp.getPC()
-#         self.ksp.setPC('lu')
-        self.ksp.setTolerances(max_it=max_iters)
-        print self.ksp.getType()
-        # self.ksp.setMonitor(self.ksp.getMonitor())
-        self.x = self.da.createGlobalVec()
-        self.ksp.solve(self.b, self.x)
 
     def plot(self, gplot):
         gplot.plot(np.real(self.field))
-        gplot.plot(np.abs(self.field))
+        # gplot.plot(np.abs(self.field))
 
-def solve(omega, epsilon, mode_pos=20, mode_order=1, d_pml=10.):
+def solve(omega, epsilon, mode_pos=10, mode_order=1, d_pml=10.):
 
     # Create the distributed array that all vectors and matrices will be 
     # based off of.
@@ -46,60 +32,68 @@ def solve(omega, epsilon, mode_pos=20, mode_order=1, d_pml=10.):
                             boundary_type=('periodic', 'periodic'))
 
     # Create the matrix used to solve for the field.
-    A = setup_matrix(da, epsilon, omega, d_pml)
+    A = _setup_matrix(da, epsilon, omega, d_pml)
 
-    # Determine current sources
-    mode = wgmode.solve(omega, epsilon[mode_pos,:], mode_order)
-    b = da.createGlobalVec()
+    # Determine current sources needed to excite a 1-way waveguide mode.
+    mode = wgmode.solve(omega, epsilon[mode_pos,:], mode_order) # Find mode.
+
+    b = da.createGlobalVec() # Create current source vector.
     b_val = da.getVecArray(b) # Obtain access to elements of b.
-    b_val[mode_pos, :] = mode.field[:] # Define location of point source.
-    # b_val[mode_pos-1, :] = -mode.field[:] * np.exp(1j * mode.beta) # Define location of point source.
-#     b_val[mode_pos+1, :] = mode.field[:] * np.exp(1j * 2 * np.pi / 9) # Define location of point source.
-    print mode.beta, 2*np.pi /mode.beta # Define location of point source.
+    b_val[mode_pos, :] = mode.field[:] # Insert mode at two locations.
+    b_val[mode_pos-1, :] = -mode.field[:] * np.exp(1j * mode.beta) 
 
 
+    # Solve the linear system.
     ksp = PETSc.KSP().create()
     ksp.setOperators(A)
-    ksp.setFromOptions()
-#         ksp.setType('preonly')
-#         print ksp.getPC()
-#         ksp.setPC('lu')
-    ksp.setTolerances(max_it=100)
-    print ksp.getType()
-    # ksp.setMonitor(ksp.getMonitor())
-    x = da.createGlobalVec()
-    ksp.solve(b, x)
-
-    return FieldSolve(da.getVecArray(x)[:])
 
 
-def setup_matrix(da, epsilon, omega, d_pml):
+    ksp.setType('preonly') # Don't use any Krylov subspace iterative algorithms.
+    pc = ksp.getPC()
+    pc.setType('lu') # Instead, solve using good ol' LU factorization.
+
+#     ksp.setType('bcgs') # Use Bi-CG-Stab algorithm.
+#     pc = ksp.getPC()
+#     pc.setType('none') # Without preconditioning.
+#     ksp.setTolerances(max_it=10000) # Set maximum iterations to 10,000.
+
+    # Note that I should add monitoring to this, but haven't been able to 
+    # figure it out (2011-09-26).
+
+    x = da.createGlobalVec() # Vector to store solution in.
+    ksp.solve(b, x) # Solve.
+
+    # Print result.
+    print "Simulated in", ksp.getIterationNumber(), "iterations,", \
+            "with residual norm of", ksp.getResidualNorm()
+
+    # Return result as a FieldSolve class instance.
+    return FieldSolve(omega, mode_order, epsilon, 
+                        da.getVecArray(x)[:], ksp.getResidualNorm())
+
+
+def _setup_matrix(da, epsilon, omega, d_pml):
     m = np.sqrt(np.max(epsilon.flatten()))
-    print m, 'm'
 
     sc_x = lambda x, y: \
-            stretch_coords(d_pml, epsilon.shape[0]-0.5, x, 1/(omega * m))
+            _stretch_coords(d_pml, epsilon.shape[0]-0.5, x, 1/(omega * m))
     sc_y = lambda x, y: \
-            stretch_coords(d_pml, epsilon.shape[1]-0.5, y, 1/(omega * m))
-
-#     for i in np.arange(epsilon.shape[0], dtype=np.float64):
-#         print sc_x(i,0),
-#     return
+            _stretch_coords(d_pml, epsilon.shape[1]-0.5, y, 1/(omega * m))
 
     # H-fields need to "reach" back since that's where corresponding E-fields
     # are located.
-    Dx_H = diff_mat(da, (-1, 0), stretch_coords=lambda x, y: sc_x(x, y))
-    Dy_H = diff_mat(da, (0, -1), stretch_coords=lambda x, y: sc_y(x, y))
+    Dx_H = _diff_mat(da, (-1, 0), stretch_coords=lambda x, y: sc_x(x, y))
+    Dy_H = _diff_mat(da, (0, -1), stretch_coords=lambda x, y: sc_y(x, y))
 
-    Dx_E = diff_mat(da, (1, 0), stretch_coords=lambda x, y: sc_x(x+0.5, y))
-    Dy_E = diff_mat(da, (0, 1), stretch_coords=lambda x, y: sc_y(x, y+0.5))
+    Dx_E = _diff_mat(da, (1, 0), stretch_coords=lambda x, y: sc_x(x+0.5, y))
+    Dy_E = _diff_mat(da, (0, 1), stretch_coords=lambda x, y: sc_y(x, y+0.5))
 
-    ex, ey = interp_epsilon(da, epsilon)
+    inv_ex, inv_ey = _get_inv_eps(da, epsilon)
 
-    Dx_H.diagonalScale(ey)
+    Dx_H.diagonalScale(inv_ey)
     A = Dx_E.matMult(Dx_H)
 
-    Dy_H.diagonalScale(ex)
+    Dy_H.diagonalScale(inv_ex)
     A.axpy(1.0, Dy_E.matMult(Dy_H))
 
     A.shift(-omega**2)
@@ -107,7 +101,7 @@ def setup_matrix(da, epsilon, omega, d_pml):
     return A
 
 
-def diff_mat(da, shift, coeff=1.0, stretch_coords=None):
+def _diff_mat(da, shift, coeff=1.0, stretch_coords=None):
     """ Create a difference matrix.
 
     """
@@ -121,8 +115,8 @@ def diff_mat(da, shift, coeff=1.0, stretch_coords=None):
     col = PETSc.Mat.Stencil() # Defines column location of grid element.
 
     (i0, i1), (j0, j1) = da.getRanges()
-    for i in np.arange(i0, i1, dtype=np.float64):
-        for j in np.arange(j0, j1, dtype=np.float64):
+    for i in range(i0, i1):
+        for j in range(j0, j1):
             row.index = (i, j)
 
             col.index = (i, j)
@@ -136,28 +130,28 @@ def diff_mat(da, shift, coeff=1.0, stretch_coords=None):
     return D
 
 
-def stretch_coords(d_pml, max_pos, pos, sigma, m=2.5):
+def _stretch_coords(d_pml, max_pos, pos, sigma, m=2.5):
     """ Generate the stretched coordinate coefficients which are used in
         the PML (perfectly matched layer) absorbing boundary.
 
     """
 
     if pos < d_pml:
-        return (1 + 1j * sigma * ((d_pml - pos) / d_pml)**m)
+        return (1 + 1j * sigma * (float(d_pml - pos) / d_pml)**m)
     elif max_pos - pos < d_pml:
-        return (1 + 1j * sigma * ((d_pml - (max_pos - pos)) / d_pml)**m)
+        return (1 + 1j * sigma * (float(d_pml - (max_pos - pos)) / d_pml)**m)
     else:
         return 1
 
-def interp_epsilon(da, epsilon):
+def _get_inv_eps(da, epsilon):
     shape = epsilon.shape
 
-    ex = da.createGlobalVec()
-    da.getVecArray(ex)[:] = 0.5 * (epsilon[:,:] + 
-                            epsilon[range(1, shape[0]) + [0], :])**-1
+    inv_ex = da.createGlobalVec()
+    da.getVecArray(inv_ex)[:] = (0.5 * (epsilon[:,:] + 
+                            epsilon[range(1, shape[0]) + [0], :]))**-1
 
-    ey = da.createGlobalVec()
-    da.getVecArray(ey)[:] = 0.5 * (epsilon[:,:] +
-                            epsilon[:, range(1, shape[1]) + [0]])**-1
+    inv_ey = da.createGlobalVec()
+    da.getVecArray(inv_ey)[:] = (0.5 * (epsilon[:,:] +
+                            epsilon[:, range(1, shape[1]) + [0]]))**-1
 
-    return ex, ey
+    return inv_ex, inv_ey
--- sim_test.py
+++ sim_test.py
@@ -2,18 +2,10 @@ from em_2dte import sim
 import numpy as np
 from plot import gnuplotter
 
-gplot = gnuplotter.create(debug_option=1)
+gplot = gnuplotter.create(debug_option=0)
 
-epsilon = np.concatenate((np.ones((60, 15)), 1*np.ones((60, 11)), 
+epsilon = np.concatenate((np.ones((60, 15)), 12.25*np.ones((60, 9)), 
                             np.ones((60, 15))), axis=1)
-sim = sim.solve(0.2, epsilon, d_pml=10, mode_pos=30)
-# sim.insert_structure(0.1)
-
-# sim.solve(0.1)
+sim = sim.solve(0.2, epsilon)
 sim.plot(gplot)
-# gplot.save('test.ps')
 raw_input('Please press return to continue...\n')
-# print sim.A.getValues(range(9), range(9))
-
-# pylab.contourf(sim.da.getVecArray(sim.x)[:])
-# pylab.show()
