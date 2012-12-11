5f6bf3cbb1b1c7190855852d2ed97a107f6333a3
Jesse Lu <mr.jesselu@gmail.com>
2012-12-11 10:43:48 -0800

    Figuring out derivatives for prodQ_local.

---
+++ optimization/paradigm/private/derivative_tester.m
@@ -0,0 +1,14 @@
+
+function [err] = test_derivative(fun, df_dz, f0, z0, step_len)
+% Check a derivative.
+    
+    % Produce a random direction.
+    dz = randn(size(z0));
+    dz = step_len * dz / norm(dz);
+
+    % Evaluate delta in that direction empirically
+    delta_empirical = fun(z0 + dz) - f0;
+    delta_derivative = real(df_dz * dz);
+
+    err = norm(delta_empirical - delta_derivative) / norm(delta_empirical);
+
--- optimization/paradigm/prodQ_local.m
+++ optimization/paradigm/prodQ_local.m
@@ -40,6 +40,42 @@ function [P, q, state] = prodQ_local(z, field_obj, phys_res, ...
 
     %% Compute df_dx
 
+    a = 1; % Normalization factor in relaxed field objective.
+    p = 2; % Exponent in relaxed field objective.
+
+    alpha = field_obj(k).alpha;
+    C = field_obj(k).C;
+    f_i = @(x) ((abs(C'*x) - alpha) < 0) .* (-abs(C'*x) + alpha);
+    f = @(x) sum(f_i(x));
+
+    df_dx = (- (p/a) * (-abs(C'*x{k}) + alpha).^(p-1) .* sign(C'*x{k}))' * C';
+
+    fun = @(x) abs(C'*x);
+    
+    df_dx = ((C'*x{k})'./abs(C'*x{k})') * C';
+
+    C = (C(:,:)); 
+    fun = @(x) sum(abs(C'*x));
+    df_dx = @(x) (conj(C'*x)./abs(C'*x)).' * C';
+
+%     f_i = @(x) ((abs(C'*x) - alpha) < 0) .* (abs(C'*x) - alpha);
+%     df_dx = @(x) diag(((abs(C'*x) - alpha) < 0) .* diag(
+    fi = @(x) ((abs(C'*x) - alpha) < 0) .* (abs(C'*x) - alpha);
+    dfi_dx = @(x) diag(((abs(C'*x) - alpha) < 0) .* conj(C'*x)./abs(C'*x)) * C';
+
+    p = 1 + abs(randn(1));
+    a = randn(1);
+    fun = @(x) sum((1/a) * (-fi(x)).^p);
+    df_dx = @(x) sum(diag((p/a) * (-fi(x)).^(p-1)) * (-dfi_dx(x)), 1);
+
+    y = x{1};
+    p
+    a
+    fi(y).^p
+    fun(y)
+    derivative_tester(fun, df_dx(y), fun(y), y, 1e-6)
+
+
     %% Compute grad_F
 
     %% Form Q(z)
