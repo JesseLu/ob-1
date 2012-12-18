e36d2642348333d3ce80052e9de35d7924cb265b
Jesse Lu <mr.jesselu@gmail.com>
2012-12-18 15:54:01 -0800

    Clean prodQ_global with only specific function handles.

--- optimization/paradigm/prodQ_global.m
+++ optimization/paradigm/prodQ_global.m
@@ -51,61 +51,9 @@ function [P, q, status] = prodQ_global(z, opt_prob, state, varargin)
     newton_max_steps = state.newton_max_steps;
     newton_err_thresh = state.newton_err_thresh; 
 
-    %% Check the gradient and Hessian of the relaxed field design objective
-    % Note the special form that the Hessian must take in order to be tested.
-% 
-%     % Function handles for calculating the gradient and Hessian.
-%     f = @(alpha, beta, C, t, phi, x) ...
-%                 -sum(ln(real(exp(-i*phi).*(C'*x)) - alpha)) + ...
-%                 -sum(ln(beta.^2 - abs(C'*x).^2));
-%     grad_f = @(alpha, beta, C, t, phi, x) ...
-%                 -sum((C * diag(exp(i*phi)./ ...
-%                                 (real(exp(-i*phi).*(C'*x)) - alpha))), 2) + ...
-%                 2 * sum(C * diag((C'*x)./(beta.^2 - abs(C'*x).^2)), 2);
-%     Hess_f = @(alpha, beta, C, t, phi, x, dx) ...
-%                 C * diag(exp(i*phi)./(real(exp(-i*phi).*(C'*x)) - alpha).^2) ...
-%                     * real(exp(-i*phi) .* (C' * dx)) + ...
-%                 C * diag(2./(beta.^2 - abs(C'*x).^2)) * C' * dx + ...
-%                     4 * C * diag((C'*x)./(beta.^2 - abs(C'*x).^2).^2) * ...
-%                     real(diag((x'*C))*C'*dx);
-% 
-%     % Test derivative for every mode.
-%     for k = 1 : N
-%         alpha = fobj(k).alpha;
-%         beta = fobj(k).beta;
-%         C = fobj(k).C;
-%         phi = angle(C' * xv{k});
-% 
-%         fi = @(x) f(alpha, beta, C, t, phi, x);
-%         grad_fi = @(x) grad_f(alpha, beta, C, t, phi, x);
-%         Hess_fi = @(x, dx) Hess_f(alpha, beta, C, t, phi, x, dx);
-% 
-%         x = xv{k};
-% 
-%         derivative_tester(fi, grad_fi(x)', fi(x), x, 1e-6, @real);
-%         hessian_tester(grad_fi, @(dx) Hess_fi(x, dx), grad_fi(x), x, 1e-6);
-%     end
-
     %% Function handles for the gradients and Hessians.
     % Compose function handles that describe the form of the function,
     % its gradient, as well as its Hessian.
-
-    % Barrier functions.
-    f_lo = @(alpha, C, phi, x) -sum(ln(real(exp(-i*phi).*(C'*x)) - alpha));
-    f_hi = @(beta, C, x) -sum(ln(beta.^2 - abs(C'*x).^2));
-
-    % Gradient of barrier functions.
-    grad_f_lo = @(alpha, C, phi, x) -sum(C * ...
-                    diag(exp(i*phi)./ (real(exp(-i*phi).*(C'*x)) - alpha)), 2);
-    grad_f_hi = @(beta, C, x) 2 * sum(C * ...
-                    diag((C'*x)./(beta.^2 - abs(C'*x).^2)), 2);
-
-    % Hessian of barrier functions.
-    Hess_f_lo = @(alpha, C, phi, x, dx) ...
-                C * diag(1./(real(exp(-i*phi).*(C'*x)) - alpha).^2) * C';
-    Hess_f_hi = @(beta, C, x, dx) ...
-                2 * C * diag(1./(beta.^2 - abs(C'*x).^2)) * C' + ...
-                4 * C * diag(abs(C'*x).^2./(beta.^2 - abs(C'*x).^2).^2) * C';
 
     for k = 1 : N
         alpha = fobj(k).alpha;
@@ -115,23 +63,7 @@ function [P, q, status] = prodQ_global(z, opt_prob, state, varargin)
 
         A = pres(k).A(z);
         b = pres(k).b(z);
-
-        f{k} = @(x) 1/t * (f_lo(alpha, C, phi, x) + f_hi(beta, C, x));
-        grad_f{k} = @(x) 1/t * (grad_f_lo(alpha, C, phi, x) + ...
-                                grad_f_hi(beta, C, x));
-        Hess_f{k} = @(x) 1/t * (Hess_f_lo(alpha, C, phi, x) + ...
-                                Hess_f_hi(beta, C, x));
-
-        
-        f{k} = @(x) 1/t * (f_lo(alpha, C, phi, x) + f_hi(beta, C, x)) + ...
-                    1/2 * norm(A*x - b + u{k})^2;
-        grad_f{k} = @(x) 1/t * (grad_f_lo(alpha, C, phi, x) + ...
-                                grad_f_hi(beta, C, x)) + ...
-                        A' * (A*x - b + u{k});
-        Hess_f{k} = @(x) 1/t * (Hess_f_lo(alpha, C, phi, x) + ...
-                                Hess_f_hi(beta, C, x)) + ...
-                        A' * A;
-        
+   
         f{k} = @(x) 1/2 * norm(A*x - b + u{k})^2 + ...
                     -1/t * sum(ln(real(exp(-i*phi).*(C'*x)) - alpha) + ...
                                 ln(beta.^2 - abs(C'*x).^2));
