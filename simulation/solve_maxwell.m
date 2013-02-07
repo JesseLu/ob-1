7b1fd81d1811a6f5aaa820cc950e2c8a04392d96
Jesse <mr.jesselu@gmail.com>
2013-02-07 11:47:01 -0800

    Fixed J = 0 bug in solve_maxwell.

--- simulation/solve_maxwell.m
+++ simulation/solve_maxwell.m
@@ -8,8 +8,14 @@ function [callback] = solve_maxwell(omega, s_prim, s_dual, mu, epsilon, J)
     dims = size(epsilon{1});
     E0 = {zeros(dims), zeros(dims), zeros(dims)};
     subplot(2, 6, 6+6);
-    cb = maxwell(omega, s_prim, s_dual, mu, epsilon, E0, J, ...
+
+    if all([J{1}(:), J{2}(:), J{3}(:)] == 0) % We know the solution to this...
+        cb = @() my_return_zeros(dims);
+    else
+        cb = maxwell(omega, s_prim, s_dual, mu, epsilon, E0, J, ...
                     num_iters, err_thresh);
+    end
+
 
     % Modify the callback.
     function [done, E, H] = my_callback(interval)
@@ -23,4 +29,8 @@ function [callback] = solve_maxwell(omega, s_prim, s_dual, mu, epsilon, J)
 end
 
 
-
+function [done, E, H] = my_return_zeros(dims)
+    done = true;
+    E = {zeros(dims), zeros(dims), zeros(dims)};
+    H = {zeros(dims), zeros(dims), zeros(dims)};
+end
